c124467e1b166ebdbe438831bd56e439289636f0
Seonho Kim <okeydokekim@gmail.com>
2021-03-03 10:16:16 +0900

    mongoDB connect

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const port = 80;
 
+const conn = require('./conn/mongooseConnect.js');
+const mongoose = require('mongoose');
+mongoose.connect(conn, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('mongoDB Connected!'))
+    .catch(error => console.log(error));
+
 app.get('/', (request, response) => {
     response.send('hello react!');
 });
